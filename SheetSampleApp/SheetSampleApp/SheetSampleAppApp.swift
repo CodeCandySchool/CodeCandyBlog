961ced2f1c865de965ee716dc889ce77df0d89df
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-03-04 11:43:24 +0900

    SwiftUIのsheet機能を用いたサンプルコードです。

--- SheetSampleApp/SheetSampleApp/SheetSampleAppApp.swift
+++ SheetSampleApp/SheetSampleApp/SheetSampleAppApp.swift
@@ -3,7 +3,7 @@
 //  SheetSampleApp
 //
 //  Created by Kensuke Nakagawa on 2023/03/02.
-//
+//  SwiftUIのsheet機能を用いたサンプルコードです。
 
 import SwiftUI
 
@@ -11,7 +11,7 @@ import SwiftUI
 struct SheetSampleAppApp: App {
     var body: some Scene {
         WindowGroup {
-            CountShowSheetView()
+            BasicSheetView()
         }
     }
 }
