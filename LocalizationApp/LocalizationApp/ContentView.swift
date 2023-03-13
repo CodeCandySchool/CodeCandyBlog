9ea5048825171820aedad2bbd3e80c3d12b00069
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-03-13 13:21:03 +0900

    Localizableファイル内で設定したローカライズ値の使用例です。

--- LocalizationApp/LocalizationApp/ContentView.swift
+++ LocalizationApp/LocalizationApp/ContentView.swift
@@ -3,7 +3,7 @@
 //  LocalizationApp
 //
 //  Created by Kensuke Nakagawa on 2023/02/24.
-//  作成したローカライズファイルの使用例です。
+//  Localizableファイル内で設定したローカライズ値の使用例です。
 
 import SwiftUI
 
@@ -19,25 +19,33 @@ struct ContentView: View {
             // 渡された値がキーに一致した時、キーに紐づいたローカライズ値を返す
             // 渡された値がキーに一致しなかった場合、渡された文字列をそのまま返す
             
+            // ✅方法1: Text自身が持つイニシャライザ
+            // 渡されたリテラルを参照して暗黙的なキー検索
             VStack {
-                // Text自身が持つイニシャライザ
-                // 渡されたリテラルを参照して暗黙的なキー検索
                 Text("date")
                 Text("message")
                 Text("codecandy") // ⬅︎キーと一致しない値
             }
             
             VStack {
-                // iOS15~のStringイニシャライザ
-                // String(localized:)
+                // 明示的に「LocalizedStringKey」でリテラル文字をラップ
+                // ⬆︎の方法と同様の結果が得られる
+                Text(LocalizedStringKey("date"))
+                Text(LocalizedStringKey("message"))
+                Text(LocalizedStringKey("codecandy")) // ⬅︎キーと一致しない値
+            }
+            
+            // ✅方法2: iOS15~のStringイニシャライザ
+            // String(localized:)
+            VStack {
                 Text(String(localized: "date"))
                 Text(String(localized: "message"))
                 Text(String(localized: "codecandy")) // ⬅︎キーと一致しない値
             }
             
+            // ✅方法3: iOS14以下, 従来のローカライズ方法
+            // NSLocalizedString
             VStack {
-                // iOS14以下, 従来のローカライズ方法
-                // NSLocalizedString
                 Text(NSLocalizedString("date", comment: ""))
                 Text(NSLocalizedString("message", comment: ""))
                 Text(NSLocalizedString("codecandy", comment: "")) // ⬅︎キーと一致しない値
