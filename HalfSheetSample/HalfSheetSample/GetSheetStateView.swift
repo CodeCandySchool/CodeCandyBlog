bccc1b929af81bd4fa1d8820df95c40f420f8b34
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-06-04 12:37:36 +0900

    sheet表示状態を取得する

--- HalfSheetSample/HalfSheetSample/GetSheetStateView.swift
+++ HalfSheetSample/HalfSheetSample/GetSheetStateView.swift
@@ -4,12 +4,60 @@
 //
 //  Created by Kensuke Nakagawa on 2023/06/04.
 //
+// シートの表示状態を監視する
 
 import SwiftUI
 
 struct GetSheetStateView: View {
+
+    @State private var isShowSheet = false
+    // シートの状態を取得するために用いるプロパティ
+    @State private var presentationDetent = PresentationDetent.medium
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        Button("シートを表示") {
+            isShowSheet.toggle()
+        }
+        .sheet(isPresented: $isShowSheet,
+               onDismiss: didDismiss) {
+
+            SheetStateDetails(show: $isShowSheet,
+                              state: presentationDetent)
+                // 第二引数「selection:」にStateプロパティを設定
+                .presentationDetents([
+                    .medium,
+                    .large,
+                ], selection: $presentationDetent) // ⬅︎
+        }
+    }
+    // シートの初期表示を固定化するためのメソッド
+    func didDismiss() {
+        presentationDetent = .medium
+    }
+}
+
+struct SheetStateDetails: View {
+
+    @Binding var show: Bool
+    let state: PresentationDetent
+
+    var body: some View {
+        VStack(spacing: 30) {
+
+            switch state {
+            case .medium:
+                Text("ハーフ画面").font(.largeTitle)
+            case .large:
+                Text("全画面").font(.largeTitle)
+            default:
+                EmptyView()
+            }
+
+            Button("シートを閉じる") {
+                show.toggle()
+            }
+        }
     }
 }
 
