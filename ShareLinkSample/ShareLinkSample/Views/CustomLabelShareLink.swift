4b218d2ffb9c49631eaf7111f7366a5fdaa9547b
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-06-07 19:20:59 +0900

    シェアボタンの外観カスタマイズ

--- ShareLinkSample/ShareLinkSample/Views/CustomLabelShareLink.swift
+++ ShareLinkSample/ShareLinkSample/Views/CustomLabelShareLink.swift
@@ -4,15 +4,31 @@
 //
 //  Created by Kensuke Nakagawa on 2023/06/05.
 //
+// シェアリンクボタンの外観をカスタマイズ
 
 import SwiftUI
 
 struct CustomLabelShareLink: View {
     var body: some View {
-        ShareLink(item: URL(
-            string: "https://developer.apple.com/xcode/swiftui/")!
-        ) {
-            Label("シェアする", systemImage: "message.fill")
+
+        VStack(spacing: 30) {
+
+//            // デフォルトのシェアボタン
+            ShareLink(item: URL(
+                string: "https://developer.apple.com/xcode/swiftui/")!
+            )
+
+            // シェアボタンのラベル全体を変更
+            ShareLink(item: URL(
+                string: "https://developer.apple.com/xcode/swiftui/")!
+            ) {
+                Label("ラベル全体を変更", systemImage: "message.fill")
+            }
+
+            // シェアボタンのタイトルのみ変更
+            ShareLink("タイトルのみ変更", item: URL(
+                string: "https://developer.apple.com/xcode/swiftui/")!
+            )
         }
     }
 }
