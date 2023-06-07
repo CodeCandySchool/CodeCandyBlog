b028e275f88002363b693bbef78511f334e74749
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-06-07 19:20:23 +0900

    カスタム構造体をシェア共有データ利用

--- ShareLinkSample/ShareLinkSample/Views/CustomDataShareLink.swift
+++ ShareLinkSample/ShareLinkSample/Views/CustomDataShareLink.swift
@@ -4,17 +4,48 @@
 //
 //  Created by Kensuke Nakagawa on 2023/06/07.
 //
+// 独自のカスタム構造体をシェア共有データとして利用する
 
 import SwiftUI
 
-struct CustomDataShareLink: View {
+// Transferableに準拠した独自のカスタムデータ
+struct Photo: Transferable {
+    static var transferRepresentation: some TransferRepresentation {
+        ProxyRepresentation(exporting: \.image)
+    }
+
+    public var image: Image
+    public var caption: String
+}
+
+// カスタムデータを用いたShareLinkサンプルビュー
+struct PhotoView: View {
+
+    // Transferableに準拠したカスタムデータ
+    var photo: Photo
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        photo.image
+            .resizable()
+            .scaledToFit()
+            .toolbar {
+                // カスタムデータをShareLinkに渡す
+                ShareLink(
+                    item: photo,
+                    preview: SharePreview(
+                        photo.caption,
+                        image: photo.image)
+                )
+            }
     }
 }
 
-struct CustomDataShareLink_Previews: PreviewProvider {
-    static var previews: some View {
-        CustomDataShareLink()
+struct CustomDataShareLinkView: View {
+    var body: some View {
+        NavigationStack {
+            PhotoView(photo: Photo(image: Image("sampleImage"),
+                                   caption: "シューズ（赤）")
+            )
+        }
     }
 }
