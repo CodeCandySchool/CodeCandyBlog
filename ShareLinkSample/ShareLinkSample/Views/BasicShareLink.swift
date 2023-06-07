947bbf2c57c3d4c6c42d831c3447e15aff9a6b14
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-06-07 19:19:34 +0900

    シェアリンクのシンプルな実装

--- ShareLinkSample/ShareLinkSample/Views/BasicShareLink.swift
+++ ShareLinkSample/ShareLinkSample/Views/BasicShareLink.swift
@@ -4,17 +4,19 @@
 //
 //  Created by Kensuke Nakagawa on 2023/06/05.
 //
+// シェアリンクのシンプルな記述
 
 import SwiftUI
 
 struct BasicShareLink: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-struct BasicShareLink_Previews: PreviewProvider {
-    static var previews: some View {
-        BasicShareLink()
+        /// URL シェア
+        ShareLink(item: URL(
+            string: "https://developer.apple.com/xcode/swiftui/")!
+        )
+
+        /// String シェア
+        ShareLink(item: "CodeCandy")
     }
 }
