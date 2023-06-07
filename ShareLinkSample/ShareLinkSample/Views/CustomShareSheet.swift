1f4c21d3bfbbd4f358aac745089ff27613eb6d31
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-06-07 19:21:41 +0900

    シェア共有シートの一部カスタマイズ

--- ShareLinkSample/ShareLinkSample/Views/CustomShareSheet.swift
+++ ShareLinkSample/ShareLinkSample/Views/CustomShareSheet.swift
@@ -4,12 +4,25 @@
 //
 //  Created by Kensuke Nakagawa on 2023/06/07.
 //
+// シェアリンクの共有シートを一部カスタマイズ
 
 import SwiftUI
 
 struct CustomShareSheet: View {
+
+    // Transferableに準拠した独自のカスタムデータ
+    let photo = Photo(image: Image("sampleImage"), caption: "シューズ")
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        // 共有オブジェクトがString値であれば、そのまま共有シート上に表示
+//        ShareLink(item: "CodeCandy")
+
+        // 共有シート用のプレビューデータを設定
+        ShareLink(item: "CodeCandy",
+                  preview: SharePreview("プレビュー用のMessage",
+                                        image: photo.image)
+        )
     }
 }
 
