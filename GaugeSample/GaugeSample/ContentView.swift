39b8b23185b3f2a528abb83c0e08672f16a75869
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-07-13 23:17:08 +0900

    Gaugeのサンプルコード

---
+++ GaugeSample/GaugeSample/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  GaugeSample
+//
+//  Created by Kensuke Nakagawa on 2023/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CustomRangeGaugeView()
+    }
+}
---
+++ GaugeSample/GaugeSample/GaugeSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugeSampleApp.swift
+//  GaugeSample
+//
+//  Created by Kensuke Nakagawa on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct GaugeSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
