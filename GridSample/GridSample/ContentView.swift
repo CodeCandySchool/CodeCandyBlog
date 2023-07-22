745b39358feaf8076b4f177019bbdb1c2072ff64
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-07-22 18:22:16 +0900

    Gridのサンプルコード

---
+++ GridSample/GridSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  GridSample
+//
+//  Created by Kensuke Nakagawa on 2023/07/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        BasicGrid()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GridSample/GridSample/GridSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridSampleApp.swift
+//  GridSample
+//
+//  Created by Kensuke Nakagawa on 2023/07/14.
+//
+
+import SwiftUI
+
+@main
+struct GridSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
