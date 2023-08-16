ddee7e787ce8b9b12ab0e734e543b135264280be
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-08-16 16:38:37 +0900

    指定範囲内でのみ、日付選択できるようにする実装

---
+++ MultiDatePickerSample/MultiDatePickerSample/Views/SelectDateRange.swift
@@ -0,0 +1,38 @@
+//
+//  SelectDateRange.swift
+//  MultiDatePickerSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/16.
+//
+
+import SwiftUI
+
+
+
+
+/// 選択できる日付を範囲指定する
+struct SelectDateRange: View {
+
+    // 環境変数「calendar」「timeZone」を用意
+    @Environment(\.calendar) var calendar
+    @Environment(\.timeZone) var timeZone
+
+    var bounds: Range<Date> {
+        // 選択日付の始まりを設定
+        let start = calendar.date(from: DateComponents(
+            timeZone: timeZone, year: 2023, month: 8, day: 20))!
+        // 選択日付の終わりを設定
+        let end = calendar.date(from: DateComponents(
+            timeZone: timeZone, year: 2023, month: 8, day: 27))!
+
+        return start ..< end
+    }
+    
+    @State private var dates: Set<DateComponents> = []
+
+    var body: some View {
+
+        // 引数「in:」に日付指定設定を渡す
+        MultiDatePicker("複数の日付選択", selection: $dates, in: bounds)
+    }
+}
