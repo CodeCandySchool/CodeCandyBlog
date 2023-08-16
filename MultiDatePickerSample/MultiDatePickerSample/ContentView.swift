e99ce130ba74ff8e257611549588c94723ce4f8b
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-08-16 16:39:08 +0900

    iOS16+のMultiDatePickerサンプルコード

---
+++ MultiDatePickerSample/MultiDatePickerSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MultiDatePickerSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        SelectDateRange()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiDatePickerSample/MultiDatePickerSample/MultiDatePickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiDatePickerSampleApp.swift
+//  MultiDatePickerSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/16.
+//
+
+import SwiftUI
+
+@main
+struct MultiDatePickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
