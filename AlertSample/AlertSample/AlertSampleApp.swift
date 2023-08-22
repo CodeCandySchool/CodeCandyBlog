158fe0c43c88880abdf61bae1aeeefed95c5d957
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-08-22 13:55:02 +0900

    iOS15+で使用可能なalertメソッドのサンプル

---
+++ AlertSample/AlertSample/AlertSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSampleApp.swift
+//  AlertSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/21.
+//
+
+import SwiftUI
+
+@main
+struct AlertSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlertSample/AlertSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AlertSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        AlertPositioningNotes()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
