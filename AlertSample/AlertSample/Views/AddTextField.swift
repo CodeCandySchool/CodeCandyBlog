57b72d584de4bd6b5321d5cff13f30b7e475145d
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-08-23 12:40:08 +0900

    iOS15+で使用可能なalertメソッドのサンプル

--- AlertSample/AlertSample/Views/AddTextField.swift
+++ AlertSample/AlertSample/Views/AddTextField.swift
@@ -7,9 +7,31 @@
 
 import SwiftUI
 
+/// iOS16+から使えるアラート + TextField and SecureField
 struct AddTextField: View {
+
+    @State private var showAlert = false
+    @State private var address = ""
+    @State private var password = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("アラートを表示する") { showAlert = true }
+        .alert("LogIn", isPresented: $showAlert) {
+
+            TextField("メールアドレス", text: $address)
+            SecureField("パスワード", text: $password)
+
+            Button("キャンセル") {
+
+            }
+            Button("OK") {
+
+            }
+
+        } message: {
+            // アラートのメッセージ...
+            Text("必要な情報を入力してください")
+        }
     }
 }
 
