6184fbf12b7f8b037ed903e221a60a4f92e8ea8d
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-08-23 12:38:22 +0900

    iOS16+から使えるアラート + TextField and SecureField

---
+++ AlertSample/AlertSample/Views/AddTextField.swift
@@ -0,0 +1,20 @@
+//
+//  AddTextField.swift
+//  AlertSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/23.
+//
+
+import SwiftUI
+
+struct AddTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTextField()
+    }
+}
