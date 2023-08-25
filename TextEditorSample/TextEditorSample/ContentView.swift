787283787c0e78a873d00489b54332e51d82b963
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-08-25 10:47:39 +0900

    複数行入力「TextEditor」の実装サンプル

---
+++ TextEditorSample/TextEditorSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TextEditorSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        FocusControlTextEditor()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextEditorSample/TextEditorSample/TextEditorSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditorSampleApp.swift
+//  TextEditorSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TextEditorSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
