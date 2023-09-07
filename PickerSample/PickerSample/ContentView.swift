2ea13cdc325556a59a4bd5bd93576174e350a3ed
Kensuke Nakagawa <kennsuke242424@gmail.com>
2023-09-07 20:55:16 +0900

    SwiftUI_Pickerの実装サンプル

---
+++ PickerSample/PickerSample/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PickerSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MenuStylePicker()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PickerSample/PickerSample/PickerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerSampleApp.swift
+//  PickerSample
+//
+//  Created by Kensuke Nakagawa on 2023/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PickerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
